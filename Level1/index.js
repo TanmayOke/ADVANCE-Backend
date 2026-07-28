4b31fc7eb61364f0bf2e960e740aca7d55ec1ef0
TanmayOke <oketanmay@gmail.com>
2026-07-28 09:58:20 +0530

    second-commit

--- Level1/index.js
+++ Level1/index.js
@@ -1,2 +1,14 @@
 import express from 'express'
 import env from 'dotenv'
+
+env.config()
+const port = process.env.PORT || 5555
+const app =express()
+
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"hello from docker"})
+})
+
+app.listen(port,()=>{
+    console.log("Hello serve started at " + port + " wapas se server start hogya")
+})
